5df22a4254b2cff15ddc7a650d4591d839b2e401
James Kennedy <acinea.kennedy@gmail.com>
2023-03-08 18:05:44 -0500

    Created footer component

---
+++ src/components/Footer/footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import logo from '../../resources/images/logo.png'
+import './footer.css'
+
+export function Footer() {
+    return (
+        <div className='footer-container'>
+            <div className='footer-content'>
+                <img src={logo} className="footer-img"alt="personal logo"/>
+                <h4>ⓒ 2023</h4>
+            </div>
+        </div>
+    )
+}
