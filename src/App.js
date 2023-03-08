5e05f9d06acbead85a62a96ea0755311a21ec889
James Kennedy <acinea.kennedy@gmail.com>
2023-03-08 07:06:15 -0500

    Setting up header functionality

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import MenuIcon from '@mui/icons-material/Menu';
 import { Header } from './components/Header/header';
 
 function App() {
--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -1,13 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
+import MenuIcon from '@mui/icons-material/Menu';
+import MenuOpenIcon from '@mui/icons-material/MenuOpen';
 import './header.css'
 
 export function Header() {
+    const [active, setActive] = useState(false);
+    const toggleMenu = () => {
+        active === false ? setActive(true) : setActive(false)
+    }
+
     return (
-        <header>
-            <div className='header-logo-container'>
-                <img src="./resources/images/logo.png" />
-                <h1>James Kennedy</h1>
-            </div>
-        </header>
+        <div>
+            <header>
+                <div className={active ? "header-menu-button-active" : "header-menu-button-inactive"}>
+                    {active ? <MenuOpenIcon  fontSize='large' onClick={toggleMenu} /> : <MenuIcon fontSize='large' onClick={toggleMenu} />}
+                </div>
+                <div className='header-logo-container'>
+                    <img src="./resources/images/logo.png" />
+                    <h1>James Kennedy</h1>
+                </div>
+                <nav className={active ? 'nav-menu-container-active' : 'nav-menu-container-inactive'}>
+                    <ul>
+                        <li>Home</li>
+                        <li>Portfolio</li>
+                        <li>Resume</li>
+                        <li>Contact</li>
+                    </ul>
+                </nav>
+            </header>
+        </div>
     )
 }
