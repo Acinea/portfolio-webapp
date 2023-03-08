890020196ffa44c6d70a8114a9f932010f3ffa15
James Kennedy <acinea.kennedy@gmail.com>
2023-03-08 18:04:59 -0500

    Added homepage, footer and styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
-import MenuIcon from '@mui/icons-material/Menu';
 import { Header } from './components/Header/header';
+import { Home } from './components/Home/home'
+import { Footer } from './components/Footer/footer'
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Home />
+      <Footer />
     </div>
   );
 }
