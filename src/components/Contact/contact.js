46b2f3ac067d5dd60b1392e2c1cfbd14b299698c
James Kennedy <acinea.kennedy@gmail.com>
2023-03-09 16:14:20 -0500

    Added contact information section

--- src/components/Contact/contact.js
+++ src/components/Contact/contact.js
@@ -33,20 +33,6 @@ export function Contact() {
                     </div>
                 </form>
             </section>
-            <section className='contact-info-container'>
-                <h2 className='contact-info'>Location</h2>
-                <h2 className='contact-info'>Phone</h2>
-                <h2 className='contact-info'>Email</h2>
-                <h2 className='contact-info'>Github</h2>
-                <p className='contact-info-border'></p>
-                <p className='contact-info-border'></p>
-                <p className='contact-info-border'></p>
-                <p className='contact-info-border'></p>
-                <h3 className='contact-info'>Old Bridge, New Jersey</h3>
-                <h3 className='contact-info'>(848) 459-0968</h3>
-                <h3 className='contact-info'>acinea.kennedy@gmail.com</h3>
-                <h3 className='contact-info'>https://github.com/Acinea</h3>
-            </section>
         </main>
     )
 }
