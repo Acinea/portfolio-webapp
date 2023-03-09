910d74ac53b3ac2a122488bac5acd31742bdbcc2
James Kennedy <acinea.kennedy@gmail.com>
2023-03-09 14:40:30 -0500

    Setup page routing with React Router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import './App.css';
+import {BrowserRouter as Router, Routes, Route, } from 'react-router-dom'
 import { Header } from './components/Header/header';
 import { Home } from './components/Home/home'
+import { Contact } from './components/Contact/contact'
 import { Footer } from './components/Footer/footer'
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Home />
-      <Footer />
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path='/' exact element={ <Home /> } />
+          <Route path='/contact' element={ <Contact /> } />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
