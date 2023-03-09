cf6aec1c700640bb1d3837b46754afa3ebb5a193
James Kennedy <acinea.kennedy@gmail.com>
2023-03-09 14:42:15 -0500

    Setup React Router Link

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import {Link} from 'react-router-dom'
 import MenuIcon from '@mui/icons-material/Menu';
 import CloseIcon from '@mui/icons-material/Close';
 import logo from '../../resources/images/logo.png'
@@ -17,10 +18,10 @@ export function Header() {
                     <MenuIcon fontSize='large' onClick={toggleMenu} />
                     {/*active ? <MenuOpenIcon  fontSize='large' onClick={toggleMenu} /> : <MenuIcon fontSize='large' onClick={toggleMenu} />*/}
                 </div>
-                <div className='header-logo-container'>
+                <Link to='/' className='nav-link'><div className='header-logo-container'>
                     <img src={logo} alt="personal-logo"/>
                     <h1>James Kennedy</h1>
-                </div>
+                </div></Link>
                 <nav className={active ? 'nav-menu-container-active' : 'nav-menu-container-inactive'}>
                     <div className="menu-title-container">
                         <div className="header-menu-button-active">
@@ -29,10 +30,11 @@ export function Header() {
                         <h2>Menu</h2>
                     </div>
                     <ul className="menu-list">
-                        <li>Home</li>
-                        <li>Portfolio</li>
-                        <li>Resume</li>
-                        <li>Contact</li>
+                        <Link to='/' className="nav-link"><li>Home</li></Link>
+                        <Link to='/' className="nav-link"><li>About Me</li></Link>
+                        <Link to='/' className="nav-link"><li>Portfolio</li></Link>
+                        <Link to='/' className="nav-link"><li>Resume</li></Link>
+                        <Link to='/contact' className="nav-link"><li>Contact</li></Link>
                     </ul>
                 </nav>
             </header>
