5fab58a700f1ae8021749cdc1a23f8a732ea470e
James Kennedy <acinea.kennedy@gmail.com>
2023-03-09 14:42:39 -0500

    Setup React Router Link & styles

--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 import downcarrot from '../../resources/images/down-carrot.png';
 import desktop from '../../resources/images/desktop-template.png';
 import './home.css';
@@ -10,7 +11,7 @@ export function Home() {
                 <div className="banner-text-container">
                     <h1>Full-Stack Engineering</h1>
                     <h2>Designing engaging digital soloutions that improve performance & user-experience</h2>
-                    <button className="banner-button">Contact Me!</button>
+                    <Link to='/contact'><button className="banner-button">Contact Me!</button></Link>
                 </div>
             </div>
             <div className='featured-work-container'>
@@ -19,9 +20,33 @@ export function Home() {
                     <img src={downcarrot} alt="down arrow" className="down-carrot" />
                 </div>
                 <div className='featured-work'>
-                    <img src={desktop} alt="desktop template"/>
-                    <img src={desktop} alt="desktop template"/>
-                    <img src={desktop} alt="desktop template"/>
+                    <div className='top'>
+                        <div className="featured-work-description">
+                            <div className='top-content'>
+                                <h1>Kenne-ddit</h1>
+                                <p>A lite Reddit Client built in React. This client extracts and organizes JSON data from the Reddit API to allow users to search for their favorite sub-reddit, and view relavent posts and comments with a simple, yet beautiful UI.</p>
+                            </div>
+                        </div>
+                        <img src={desktop} className="desktop-image" alt="desktop template"/>
+                    </div>
+                    <div className='mid'>
+                        <img src={desktop} className="desktop-image" alt="desktop template"/>
+                        <div className="featured-work-description">
+                            <div className='mid-content'>
+                                <h1>Portfolio Webapp</h1>
+                                <p>This website is built on React! What initially started as an HTML/CSS site has been updated as a React App.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='bot'>
+                        <div className="featured-work-description">
+                            <div className='bot-content'>
+                                <h1>Python U.S Medical Insurance Analysis</h1>
+                                <p>Using Jupyter Notebook and Pyton3, this file contains the results of extracting and organizing raw Medical Insurance Data from Kaggle Datasets.</p>
+                            </div>
+                        </div>
+                        <img src={desktop} className="desktop-image" alt="desktop template"/>
+                    </div>
                 </div>
             </div>
         </div>
