00529db960ef7a5f8a144fbfaf73b6b0af086ce0
James Kennedy <acinea.kennedy@gmail.com>
2023-03-11 15:35:10 -0500

    Included link to contact form

--- src/components/Resume/resume.js
+++ src/components/Resume/resume.js
@@ -1,12 +1,19 @@
 import React from 'react'
-import './resume.css'
+import { Link } from 'react-router-dom'
 import file from './resume.pdf'
+import downcarrot from '../../resources/images/down-carrot.png'
+import './resume.css'
 
 export function Resume() {
 
     return (
         <main className="resume-container">
-            <iframe src={file} width="1000vw" height="800vh" type="<!DOCTYPE html>" />
+            <iframe src={file} width="1000vw" height="800vh" title="Resume" />
+            <div className='contact-form-title'>
+                <h1>Lets Connect</h1>
+                <img src={downcarrot} alt="down arrow" className="down-carrot" />
+                <Link to='/contact'><button className="banner-button">Contact Me!</button></Link>
+            </div>
         </main>
     )
 }
